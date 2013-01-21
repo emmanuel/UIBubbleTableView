930534bd026b9ad632d24b8f0defa7b984763b90
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2013-01-20 23:52:37 -0800

    Break up SGBubbleTableViewHeaderCell into smaller methods.

--- src/SGBubbleTableViewHeaderCell.m
+++ src/SGBubbleTableViewHeaderCell.m
@@ -11,6 +11,7 @@
 @interface SGBubbleTableViewHeaderCell ()
 
 @property (nonatomic, retain) UILabel *label;
+@property (nonatomic, retain) NSDateFormatter *dateFormatter;
 
 @end
 
@@ -24,34 +25,45 @@
     return 28.0;
 }
 
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self)
+    {
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
+        self.dateFormatter = [self createDateFormatter];
+        self.label = [self createLabel];
+        [self addSubview:self.label];
+    }
+
+    return self;
+}
+
 - (void)setDate:(NSDate *)value
+{
+    self.label.text = [self.dateFormatter stringFromDate:value];
+}
+
+- (NSDateFormatter *)createDateFormatter
 {
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     [dateFormatter setDateStyle:NSDateFormatterMediumStyle];
     [dateFormatter setTimeStyle:NSDateFormatterShortStyle];
-    NSString *text = [dateFormatter stringFromDate:value];
-#if !__has_feature(objc_arc)
-    [dateFormatter release];
-#endif
 
-    if (self.label)
-    {
-        self.label.text = text;
-        return;
-    }
-    
-    self.selectionStyle = UITableViewCellSelectionStyleNone;
-    self.label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, [SGBubbleTableViewHeaderCell height])];
-    self.label.text = text;
-    self.label.font = [UIFont boldSystemFontOfSize:12];
-    self.label.textAlignment = UITextAlignmentCenter;
-    self.label.shadowOffset = CGSizeMake(0, 1);
-    self.label.shadowColor = [UIColor whiteColor];
-    self.label.textColor = [UIColor darkGrayColor];
-    self.label.backgroundColor = [UIColor clearColor];
-    [self addSubview:self.label];
+    return dateFormatter;
 }
 
+- (UILabel *)createLabel
+{
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, [SGBubbleTableViewHeaderCell height])];
+    label.font = [UIFont boldSystemFontOfSize:12];
+    label.textAlignment = UITextAlignmentCenter;
+    label.shadowOffset = CGSizeMake(0, 1);
+    label.shadowColor = [UIColor whiteColor];
+    label.textColor = [UIColor darkGrayColor];
+    label.backgroundColor = [UIColor clearColor];
 
+    return label;
+}
 
 @end
